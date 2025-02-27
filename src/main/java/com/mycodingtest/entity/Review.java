04955592a4cac89873a7f6c3ad3417d37dc7b760
Seonghun Jeong <tjdgns5506@gmail.com>
2025-02-27 15:38:51 +0900

    refactor: Review 에 롬복 적용과 캡슐화 위한 setter대체 메서드 추가

--- src/main/java/com/mycodingtest/entity/Review.java
+++ src/main/java/com/mycodingtest/entity/Review.java
@@ -1,65 +1,48 @@
 package com.mycodingtest.entity;
 
+import com.mycodingtest.exception.InvalidOwnershipException;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private int difficultyLevel = -1;
+
     private int importanceLevel = -1;
+
+    private boolean reviewed;
+
+    private LocalDateTime reviewedAt;
+
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
-    private boolean reviewed;
-    private LocalDateTime reviewedAt;
 
     public Review(User user) {
         this.user = user;
     }
 
-    protected Review() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public int getDifficultyLevel() {
-        return difficultyLevel;
-    }
-
-    public void setDifficultyLevel(int difficultyLevel) {
+    public void updateRatingLevels(int difficultyLevel, int importanceLevel) {
         this.difficultyLevel = difficultyLevel;
-    }
-
-    public int getImportanceLevel() {
-        return importanceLevel;
-    }
-
-    public void setImportanceLevel(int importanceLevel) {
         this.importanceLevel = importanceLevel;
     }
 
-    public boolean isReviewed() {
-        return reviewed;
+    public void validateOwnership(Long userId) {
+        if (!this.user.getId().equals(userId)) {
+            throw new InvalidOwnershipException("User " + userId + " does not own this review");
+        }
     }
 
-    public void setReviewed(boolean reviewed) {
-        this.reviewed = reviewed;
-    }
-
-    public LocalDateTime getReviewedAt() {
-        return reviewedAt;
-    }
-
-    public void setReviewedAt(LocalDateTime reviewedAt) {
-        this.reviewedAt = reviewedAt;
-    }
-
-    public User getUser() {
-        return user;
+    public void completeReview() {
+        reviewed = true;
+        reviewedAt = LocalDateTime.now();
     }
 }
