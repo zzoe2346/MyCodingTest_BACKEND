c25749e072373028a0373c5671d4c12b4f80aff2
Seonghun Jeong <tjdgns5506@gmail.com>
2025-02-28 14:36:37 +0900

    refactor: Rename S3Service to StorageService and update references across the codebase

--- src/main/java/com/mycodingtest/review/SourceCodeService.java
+++ src/main/java/com/mycodingtest/review/SourceCodeService.java
@@ -1,22 +1,22 @@
 package com.mycodingtest.review;
 
-import com.mycodingtest.storage.S3Service;
+import com.mycodingtest.storage.StorageService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SourceCodeService {
 
-    private final S3Service s3Service;
+    private final StorageService storageService;
 
-    public SourceCodeService(S3Service s3Service) {
-        this.s3Service = s3Service;
+    public SourceCodeService(StorageService storageService) {
+        this.storageService = storageService;
     }
 
     public String getCodeUpdateUrl(String submissionId, Long userId) {
-        return s3Service.getCodeUpdateUrl(submissionId, userId);
+        return storageService.getCodeUpdateUrl(submissionId, userId);
     }
 
     public String getCodeReadUrl(String submissionId, Long userId) {
-        return s3Service.getCodeReadUrl(submissionId, userId);
+        return storageService.getCodeReadUrl(submissionId, userId);
     }
 }
--- src/main/java/com/mycodingtest/storage/impl/S3Service.java
+++ src/main/java/com/mycodingtest/storage/impl/S3Service.java
@@ -1,5 +1,6 @@
-package com.mycodingtest.storage;
+package com.mycodingtest.storage.impl;
 
+import com.mycodingtest.storage.StorageService;
 import io.awspring.cloud.s3.S3Template;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -8,7 +9,7 @@ import java.time.Duration;
 import java.util.List;
 
 @Service
-public class S3Service {
+public class S3Service implements StorageService {
 
     private final S3Template s3Template;
     @Value("${aws.s3.bucketName}")
@@ -20,16 +21,19 @@ public class S3Service {
         this.s3Template = s3Template;
     }
 
+
     public String getCodeReadUrl(String submissionId, Long userId) {
         String key = userId + "/codes/" + submissionId + ".txt";
         return s3Template.createSignedGetURL(bucketName, key, Duration.ofSeconds(urlExpirationSeconds)).toString();
     }
 
+    @Override
     public String getCodeUpdateUrl(String submissionId, Long userId) {
         String key = userId + "/codes/" + submissionId + ".txt";
         return s3Template.createSignedPutURL(bucketName, key, Duration.ofSeconds(urlExpirationSeconds)).toString();
     }
 
+    @Override
     public String getMemoReadUrl(String reviewId, Long userId) {
         String key = userId + "/memos/" + reviewId + ".txt";
         if (s3Template.objectExists(bucketName, key)) {
@@ -38,12 +42,13 @@ public class S3Service {
         return "noMemo";
     }
 
+    @Override
     public String getMemoUpdateUrl(String reviewId, Long userId) {
         String key = userId + "/memos/" + reviewId + ".txt";
         return s3Template.createSignedPutURL(bucketName, key, Duration.ofSeconds(urlExpirationSeconds)).toString();
     }
 
-
+    @Override
     public void deleteCodes(List<String> submissionIdList, Long userId) {
         for (String submissionId : submissionIdList) {
             String key = userId + "/codes/" + submissionId + ".txt";
@@ -51,6 +56,7 @@ public class S3Service {
         }
     }
 
+    @Override
     public void deleteMemo(String reviewId, Long userId) {
         String key = userId + "/memos/" + reviewId + ".txt";
         s3Template.deleteObject(bucketName, key);
