50eaa1bf0b649e3ba0a580ccdcef42525e13da07
Seonghun Jeong <tjdgns5506@gmail.com>
2025-03-01 19:49:50 +0900

    feat: CookieUtil 추가(쿠키 생성, 추출, 삭제)

---
+++ src/main/java/com/mycodingtest/common/util/CookieUtil.java
@@ -0,0 +1,43 @@
+package com.mycodingtest.common.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieUtil {
+
+    @Value("${cookie.name}")
+    private String cookieName;
+
+    public String getJwtFromCookie(HttpServletRequest request) {
+        for (Cookie cookie : request.getCookies()) {
+            if (cookie.getName().equals(cookieName)) {
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+
+    public ResponseCookie generateJwtCookie(String token) {
+        return ResponseCookie.from(cookieName, token)
+                .httpOnly(true)
+                .maxAge(60 * 60 * 24 * 90)
+                .path("/")
+                .sameSite("None")
+                .secure(true)
+                .build();
+    }
+
+    public ResponseCookie clearJwtCookie() {
+        return ResponseCookie.from(cookieName, null)
+                .httpOnly(true)
+                .maxAge(0)
+                .path("/")
+                .sameSite("None")
+                .secure(true)
+                .build();
+    }
+}
