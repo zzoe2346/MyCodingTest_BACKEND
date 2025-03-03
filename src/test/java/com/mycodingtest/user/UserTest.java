f9352e94aea94b2f7ef3885b8cbfa7dc3752987a
Seonghun Jeong <tjdgns5506@gmail.com>
2025-03-03 12:37:50 +0900

    test: User class 생성자 단위 테스트 추가

---
+++ src/test/java/com/mycodingtest/user/UserTest.java
@@ -0,0 +1,35 @@
+package com.mycodingtest.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+
+    @Test
+    void testNoArgConstructor() {
+        // when
+        User user = new User();
+
+        // then
+        assertThat(user.getId()).isNull();
+        assertThat(user.getName()).isNull();
+        assertThat(user.getOauthProvider()).isNull();
+        assertThat(user.getCreatedAt()).isNull();
+        assertThat(user.getEmail()).isNull();
+    }
+
+    @Test
+    void testParamArgConstructor() {
+        // when
+        User user = new User("Bell", "test@test.com", "picture", "google", "1122");
+
+        // then
+        // @CreationTimestamp 은 데이터베이스에 insert의 시간을 기록. 단위테스트에서는 null이 정상
+        assertThat(user.getId()).isNull();
+        assertThat(user.getName()).isEqualTo("Bell");
+        assertThat(user.getOauthProvider()).isEqualTo("google");
+        assertThat(user.getCreatedAt()).isNull();
+        assertThat(user.getEmail()).isEqualTo("test@test.com");
+    }
+}
