4d9d73f7fced4955b4506cada3ab28fa38ed9c55
Seonghun Jeong <tjdgns5506@gmail.com>
2025-03-04 15:58:26 +0900

    remove: 사용안하는 DTO 제거

--- src/main/java/com/mycodingtest/review/dto/ReviewStatusUpdateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.mycodingtest.review.dto;
-
-import jakarta.validation.constraints.NotNull;
-
-public record ReviewStatusUpdateRequest(
-        @NotNull
-        boolean reviewed
-) {
-}
