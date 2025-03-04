db7ef7a391cf8f6f7c909294ad7f88c55b3e9d59
Seonghun Jeong <tjdgns5506@gmail.com>
2025-03-04 21:47:29 +0900

    remove: 불필요한 import 문 제거

--- src/main/java/com/mycodingtest/MyCodingTestApplication.java
+++ src/main/java/com/mycodingtest/MyCodingTestApplication.java
@@ -1,10 +1,7 @@
 package com.mycodingtest;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.TimeZone;
 
 @SpringBootApplication
 public class MyCodingTestApplication {
