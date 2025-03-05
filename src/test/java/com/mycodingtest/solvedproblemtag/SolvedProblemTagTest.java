624313e2dc79af47330a472571a951696b3a9507
Seonghun Jeong <tjdgns5506@gmail.com>
2025-03-05 18:42:41 +0900

    test: SolvedProblemTag 및 SolvedProblemTagId의 no args 생성자와 동등성 테스트 추가

--- src/test/java/com/mycodingtest/solvedproblemtag/SolvedProblemTagTest.java
+++ src/test/java/com/mycodingtest/solvedproblemtag/SolvedProblemTagTest.java
@@ -48,11 +48,29 @@ class SolvedProblemTagTest {
     }
 
     @Test
-    @DisplayName("no args 생성자로 객체가 생성되야한다.")
-    void noArgConstructor() {
+    @DisplayName("solvedProblemTag는 no args 생성자로 객체가 생성되야한다.")
+    void noArgConstructorSolvedProblemTag() {
         // given
         SolvedProblemTag solvedProblemTag = new SolvedProblemTag();
         // when then
         assertThat(solvedProblemTag).isNotNull();
+    }
+
+    @Test
+    @DisplayName("solvedProblemTagId 또한 no args 생성자로 객체가 생성되야한다.")
+    void noArgConstructorSolvedProblemTagId() {
+        // given
+        SolvedProblemTag.SolvedProblemTagId id = new SolvedProblemTag.SolvedProblemTagId();
+        // when then
+        assertThat(id).isNotNull();
+    }
+
+    @Test
+    @DisplayName("solvedProblemTagId 비교시 같은 레퍼런스끼리 비교는 항상 true")
+    void solvedProblemTagId_When_Same() {
+        // given
+        SolvedProblemTag.SolvedProblemTagId id = new SolvedProblemTag.SolvedProblemTagId();
+        // when then
+        assertThat(id.equals(id)).isTrue();
     }
 }
