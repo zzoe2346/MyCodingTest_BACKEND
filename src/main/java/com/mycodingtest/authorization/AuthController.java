3bb9cc85be6b3378daf0c98eea14f3c0a3f4b01b
Seonghun Jeong <tjdgns5506@gmail.com>
2025-03-08 22:51:14 +0900

    refactor: AuthController의 API 설명을 보다 명확하게 수정

--- src/main/java/com/mycodingtest/authorization/AuthController.java
+++ src/main/java/com/mycodingtest/authorization/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Tag(name = "인증", description = "회원가입 및 로그인 관련 API")
+@Tag(name = "인증", description = "유저의 인증 상태에 관한 API")
 public class AuthController {
 
     private final AuthService authService;
@@ -21,14 +21,14 @@ public class AuthController {
     }
 
     @GetMapping("/api/sign-out")
-    @Operation(summary = "로그아웃")
+    @Operation(summary = "요청시, 로그아웃이 된다. 발급된 쿠키가 만료되는 로직이 작동함.")
     public ResponseEntity<Void> signOut(HttpServletResponse response) {
         authService.signOut(response);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/api/me")
-    @Operation(summary = "로그인 상태 체크. 401 응답시 비로그인 상태")
+    @Operation(summary = "요청시, 로그인를 상태 체크한다. 200 응답: 로그인 상태  401 응답: 비로그인 상태이다. DB Hit 없이 쿠키의 유효성을 확인함.")
     public ResponseEntity<UserInfoResponse> checkMe(@AuthenticationPrincipal CustomUserDetails userDetails) {
         return ResponseEntity.ok(authService.getUserInfo(userDetails.getPicture(), userDetails.getUsername()));
     }
