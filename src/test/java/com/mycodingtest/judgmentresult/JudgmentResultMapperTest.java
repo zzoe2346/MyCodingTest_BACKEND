9b0188b38c131ea8645574874b709f2d6eea1e6b
Seonghun Jeong <tjdgns5506@gmail.com>
2025-03-10 15:11:18 +0900

    test: JudgmentResultMapper의 인스턴스 생성 방지에 대한 단위테스트 추가

--- src/test/java/com/mycodingtest/judgmentresult/JudgmentResultMapperTest.java
+++ src/test/java/com/mycodingtest/judgmentresult/JudgmentResultMapperTest.java
@@ -1,25 +1,24 @@
 package com.mycodingtest.judgmentresult;
 
-import com.mycodingtest.review.ReviewMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class JudgmentResultMapperTest {
 
-//    @Test
-//    @DisplayName("JudgmentResultMapper는 유틸리티성 클래스이므로 인스턴스 생성이 불가능하다")
-//    void testPrivateConstructor() throws Exception {
-//        //given
-//        Constructor<JudgmentResultMapper> constructor = JudgmentResultMapper.class.getDeclaredConstructor();
-//        constructor.setAccessible(true);
-//
-//        //when then
-//        assertThrows(InvocationTargetException.class, constructor::newInstance);
-//    }
+    @Test
+    @DisplayName("JudgmentResultMapper는 유틸리티성 클래스이므로 인스턴스 생성이 불가능하다")
+    void testPrivateConstructor() throws Exception {
+        //given
+        Constructor<JudgmentResultMapper> constructor = JudgmentResultMapper.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+
+        //when then
+        assertThrows(InvocationTargetException.class, constructor::newInstance);
+    }
 
 }
