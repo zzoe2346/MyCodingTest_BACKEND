3037e4341f6e6761b64298a07b4382f8584852ec
Seonghun Jeong <tjdgns5506@gmail.com>
2025-03-16 00:08:29 +0900

    test: add unit tag to contextLoads test method

--- src/test/java/com/mycodingtest/MyCodingTestApplicationTests.java
+++ src/test/java/com/mycodingtest/MyCodingTestApplicationTests.java
@@ -1,5 +1,6 @@
 package com.mycodingtest;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MyCodingTestApplicationTests {
 
     @Test
+    @Tag("unit")
     void contextLoads() {
         MyCodingTestApplication.main(new String[]{});
     }
