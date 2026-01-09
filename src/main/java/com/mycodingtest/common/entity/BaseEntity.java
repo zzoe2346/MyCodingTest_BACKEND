907fab1d6081673114bc071a47a5ef031e3483b6
Seonghun Jeong <tjdgns5506@gmail.com>
2026-01-09 19:36:52 +0900

    feat: BaseEntity 추가
    - 엔티티에 공통으로 쓰이는 필드를 상속을 통해 해결하기 위한 추상 클래스

---
+++ src/main/java/com/mycodingtest/common/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.mycodingtest.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
