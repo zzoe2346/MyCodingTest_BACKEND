22577670178096fd5affdc519f6fc57fb37fe590
Seonghun Jeong <tjdgns5506@gmail.com>
2026-01-11 17:21:50 +0900

    docs: judgment 도메인 패키지 주석 상세히 추가

--- src/main/java/com/mycodingtest/judgment/domain/JudgmentStatus.java
+++ src/main/java/com/mycodingtest/judgment/domain/JudgmentStatus.java
@@ -1,5 +1,20 @@
 package com.mycodingtest.judgment.domain;
 
+/**
+ * <h3>채점 결과 상태 (JudgmentStatus)</h3>
+ * <p>
+ * 문제 풀이의 성공 여부를 나타내는 열거형입니다.
+ * 시스템 내부적으로 채점 결과의 종류를 구분하는 기준이 됩니다.
+ * </p>
+ */
 public enum JudgmentStatus {
-    SUCCESS, FAIL, COMPILE_ERROR
+    /**
+     * 정답
+     */
+    SUCCESS,
+
+    /**
+     * 오답
+     */
+    FAIL
 }
