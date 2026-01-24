7e029038c06686fc8104371d78d542baedca1bfb
Seonghun Jeong <tjdgns5506@gmail.com>
2026-01-24 15:51:31 +0900

    test: 테스트에 위치한 data.sql에 테스트용 유저 추가

---
+++ module-api/src/test/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO "user" (id, email, name, picture, oauth_provider, oauth_id, created_at)
+VALUES (1, 'test@example.com', 'TestUser', 'https://via.placeholder.com/150', 'google', '123456789', CURRENT_TIMESTAMP());
