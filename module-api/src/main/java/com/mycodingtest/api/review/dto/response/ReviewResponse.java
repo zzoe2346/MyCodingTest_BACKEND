77453728090410b23956af6cd2f5eb822695a22e
Seonghun Jeong <tjdgns5506@gmail.com>
2026-01-25 09:43:04 +0900

    fix: 누락된 DTO 필드 추가

--- module-api/src/main/java/com/mycodingtest/api/review/dto/response/ReviewResponse.java
+++ module-api/src/main/java/com/mycodingtest/api/review/dto/response/ReviewResponse.java
@@ -6,17 +6,32 @@ import com.mycodingtest.domain.review.ReviewStatus;
 import java.time.LocalDateTime;
 
 public record ReviewResponse(
+        Long id,
+        Long userId,
+        Long problemId,
+        Boolean isFavorite,
+        String sourceCode,//프론트에서 sourceCode로 받음
+        String content,
         int difficultyLevel,
         int importanceLevel,
         ReviewStatus status,
-        LocalDateTime reviewedAt) {
+        LocalDateTime reviewedAt,
+        boolean reviewed) {
 
     public static ReviewResponse from(ReviewInfo info) {
+
         return new ReviewResponse(
+                info.id(),
+                info.userId(),
+                info.problemId(),
+                info.isFavorite(),
+                info.revisedCode(),
+                info.content(),
                 info.difficultyLevel(),
                 info.importanceLevel(),
                 info.status(),
-                info.reviewedAt()
+                info.reviewedAt(),
+                info.reviewed()
         );
     }
 }
